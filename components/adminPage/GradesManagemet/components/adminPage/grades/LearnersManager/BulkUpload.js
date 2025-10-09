e355f5628587999715876c417f9a52bd7b91a3aa
kayjee <Kagiso.killagram@gmail.com>
2025-10-09 08:26:22 +0200

    updates

--- components/adminPage/GradesManagemet/components/adminPage/grades/LearnersManager/BulkUpload.js
+++ components/adminPage/GradesManagemet/components/adminPage/grades/LearnersManager/BulkUpload.js
@@ -286,7 +286,7 @@ const BulkUpload = ({ isOpen, onClose, selectedGrade, onUploadSuccess, schools }
     setErrorStatus('');
 
     try {
-      const response = await fetch('http://localhost:4000/api/v1/learners/bulk_upload', {
+      const response = await fetch('https://sho-backend-v2.onrender.com/api/v1/learners/bulk_upload', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ data: validationResults.dataToUpload }),
