ba4af29d6da8758c604089c0c933851c94fbc5b9
kayjee <Kagiso.killagram@gmail.com>
2025-11-13 08:58:23 +0200

    update

---
+++ pages/data-deletion.js
@@ -0,0 +1,16 @@
+export default function DataDeletion() {
+  return (
+    <div className="max-w-3xl mx-auto p-6">
+      <h1 className="text-2xl font-bold mb-4">User Data Deletion</h1>
+      <p>
+        If you would like to delete your account and personal data from SchoolHeadOffice.co.za,
+        please email us at <a href="mailto:support@schoolheadoffice.co.za" className="text-blue-600 underline">
+        support@schoolheadoffice.co.za
+        </a> with the subject line "Data Deletion Request".
+      </p>
+      <p className="mt-4">
+        We will process your request and confirm deletion of your data within 7 business days.
+      </p>
+    </div>
+  );
+}
