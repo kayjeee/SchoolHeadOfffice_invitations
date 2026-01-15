e33b7477d303e3fd1260016b46414c4dd2fae812
kayjee <Kagiso.killagram@gmail.com>
2026-01-16 00:25:32 +0200

    Update userSyncService.ts

--- lib/services/userSyncService.ts
+++ lib/services/userSyncService.ts
@@ -23,7 +23,7 @@ export async function syncUserWithRails(
     throw new Error('Auth0 user with a subject ID is required for synchronization.');
   }
 
-  const url = `${RAILS_API_URL}/api/v1/users`;
+  const url = `${RAILS_API_URL}/api/v1/users2`;
 
   const payload = {
     user: {
