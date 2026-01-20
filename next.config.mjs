8c5f7582e7cf956897813529523ddbca9a05b2c7
kayjee <Kagiso.killagram@gmail.com>
2026-01-20 12:57:20 +0200

    Add global Cache-Control headers to Next.js config
    Configured a custom headers function in next.config.mjs to set 'Cache-Control' headers for all routes, disabling caching. This helps ensure users always receive the latest content.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 // next.config.mjs
 export default {
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value:
+              'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+          },
+        ],
+      },
+    ];
+  },
 };
